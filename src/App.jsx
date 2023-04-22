56fa0223cd0f9eea2bb291d997ddeb2b5044f3c2
Bradley Griffin <bigriff11@gmail.com>
2023-04-22 09:37:46 -0600

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import dohn from './assets/dohn.png'
+import Container from './components/Conatiner'
+
+function App() {
+  
+  return (
+    <div className=" bg-neutral-800 flex justify-start">
+      <img src={dohn} alt="Business man in black and white"  className=' max-w-lg rounded-2xl m-6'/>
+      <Container/>
+    </div>
+  )
+}
+
+export default App
